a6b113dc5d4b06da02ed5b800f06ef07f5aba139
Samuel <samuelgiorgioaugustogalli.com>
2025-02-28 17:29:18 +0100

    work

--- esercizio 3/index.js
+++ esercizio 3/index.js
@@ -2,24 +2,25 @@
 let forma = prompt("inserisci la forma di cui vuoi calcolare l' area: rettangolo, triangolo, cerchio")
 console.log(forma)
 
-//Imposta le regole per le forme geometriche
-if (forma === "rettangolo") {  
+//2. Imposta le regole per le forme geometriche
+if (forma === "rettangolo") {
     //logica calcolo rettangolo 
-console.log("calcolo forma rettangolo IN")
-const base = prompt("inserisci il valore della base") 
-const altezza = prompt("inserisci il valore dell' altezza") 
-console.log(`calcolo forma rettangolo VALORI B: ${base} H:  ${altezza}`)
-//calcolo aerea rettangolo
-const area = (base * altezza)
-console.log("l' area del tuo rettangolo è: " + area)
-document.querySelector("#risultato").innerHTML = area
+    console.log("calcolo forma rettangolo IN")
+    const base = prompt("inserisci il valore della base")
+    const altezza = prompt("inserisci il valore dell' altezza")
+    console.log(`calcolo forma rettangolo VALORI B: ${base} H:  ${altezza}`)
+    //3. calcolo aerea rettangolo
+    const area = (base * altezza)
+    console.log("l' area del tuo rettangolo è: " + area)
+    document.querySelector("#risultato").innerHTML = "La tua area è " + area
+
 
 
 }
-  if (forma === "triangolo")  {
+if (forma === "triangolo") {
     //logica calcolo area triangolo
-    console.log("calcolo forma triangolo IN") 
-  }
+    console.log("calcolo forma triangolo IN")
+}
 if (forma === "cerchio") {
     //logica calcolo area cerchio
     console.log("calcolo forma cerchio IN")
