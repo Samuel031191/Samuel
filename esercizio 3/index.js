48aa475883a60d8c0a1bc323664ed5bc75d09314
Samuel <samuelgiorgioaugustogalli.com>
2025-02-28 17:06:32 +0100

    work

---
+++ esercizio 3/index.js
@@ -0,0 +1,26 @@
+// 1. Chiedi all'utente il tipo di forma geometrica
+let forma = prompt("inserisci la forma di cui vuoi calcolare l' area: rettangolo, triangolo, cerchio")
+console.log(forma)
+
+//Imposta le regole per le forme geometriche
+if (forma === "rettangolo") {  
+    //logica calcolo rettangolo 
+console.log("calcolo forma rettangolo IN")
+const base = prompt("inserisci il valore della base") 
+const altezza = prompt("inserisci il valore dell' altezza") 
+console.log(`calcolo forma rettangolo VALORI B: ${base} H:  ${altezza}`)
+//calcolo aerea rettangolo
+const area = (base * altezza)
+console.log("l' area del tuo rettangolo è: " + area)
+document.querySelector("#risultato").innerHTML = area
+
+
+}
+  if (forma === "triangolo")  {
+    //logica calcolo area triangolo
+    console.log("calcolo forma triangolo IN") 
+  }
+if (forma === "cerchio") {
+    //logica calcolo area cerchio
+    console.log("calcolo forma cerchio IN")
+}
