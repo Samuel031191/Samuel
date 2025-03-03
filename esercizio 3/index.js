769442584d74d0f49805a2b0d96deb12d32e26c0
Samuel <samuelgiorgioaugustogalli.com>
2025-03-03 14:15:01 +0100

    work

--- esercizio 3/index.js
+++ esercizio 3/index.js
@@ -19,10 +19,23 @@ if (forma === "rettangolo") {
 } else if (forma === "triangolo") {
     //logica calcolo area triangolo
     console.log("calcolo forma triangolo IN")
+    const base = prompt("inserisci il valore della base")
+    const altezza = prompt("inserisci il valore dell' altezza")
+    console.log(`calcolo forma triangolo VALORI B: ${base} H:  ${altezza}`)
+    //3. calcolo aerea triangolo
+    const area = ((base * altezza) / 2)
+    console.log("l' area del tuo triangolo è: " + area)
+    document.querySelector("#risultato").innerHTML = "La tua area è " + area
 }
 else if (forma === "cerchio") {
     //logica calcolo area cerchio
     console.log("calcolo forma cerchio IN")
+    const raggio = prompt("inserisci il valore del raggio")
+    console.log(`calcolo forma rettangolo VALORI r: ${raggio}`)
+    //3. calcolo aerea cerchio
+    const area = ((raggio * raggio) * 3.14)
+    console.log("l' area del tuo cerchio è: " + area)
+    document.querySelector("#risultato").innerHTML = "La tua area è " + area
 }
 else {
     console.log("errore")
