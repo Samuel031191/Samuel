5c142429b2d94a0bed82745d8c1362cc7cdc2f3e
Samuel <samuelgiorgioaugustogalli.com>
2025-03-03 17:32:08 +0100

    work

--- esercizio 4/index.js
+++ esercizio 4/index.js
@@ -17,8 +17,14 @@ btnCambia.addEventListener("click", function () {
     } else {
         document.querySelector("#errore").innerHTML = " Inserisci del testo prima di cambiare il contenuto!";
     }
-  });
   
+  // Funzione che cambia il colore del paragrafo
+   function cambiaColore() {
+    // Ottieni il valore selezionato dal menu a tendina
+    var coloreSelezionato = document.getElementById("colorSelect").value;
+    
+    // Cambia il colore del paragrafo
+    document.getElementById("testo").style.color = coloreSelezionato;}});
 
 
 // 3. Aggiungi un altro listener per il click sul bottone
@@ -26,11 +32,13 @@ btnCambiaTitolo.addEventListener("click", function () {
     // Prendi il testo dall'input
     const nuovoContenuto = inputTitolo.value;
 
-    // Se il testo non è vuoto, cambia il paragrafo
+    // Se il testo non è vuoto, cambia il titolo
     if (nuovoContenuto.trim() !== "") {
         titolo.textContent = nuovoContenuto;
     } else {
         document.querySelector("#errore").innerHTML = " Inserisci del testo prima di cambiare il contenuto!";
     }
   });
+
+   
   
